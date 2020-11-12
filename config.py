8943116984d8cedbc21132969d2814676244b748
Gavitana <vitalya.gaynutdinov@gmail.com>
2020-11-12 17:37:38 +0700

    Use pandas , instead of python scripts

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 LABELS = ["kazahstan", "evraz-conveyor", "ЦифровойСоветчик", "РЖД-Щебень", "НКТ", "evraz", "НИК", "kola"]
+OTHER_LABELS = ["dev","detector","prr","box","msc"]
 TIME_LABELS = {"2 часа":2, "4 часа":4, "8 часов":8, "16 часов":16}
